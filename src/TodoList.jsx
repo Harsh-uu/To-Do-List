e41c6c304703bc90c51751da63d6a459149121a5
Harsh <harshguptaaa17@gmail.com>
2023-06-21 23:39:15 +0530

    Title fixed

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,10 +3,10 @@ import { RiDeleteBin6Line } from "react-icons/ri";
 
 function TodoList(props) {
   return (
-    <nav className='flex justify-center text-gray-800 my-2'>
+    <nav className='flex justify-center text-gray-800 my-2 '>
     <li className="list-none max-w-lg w-[50%] mx-0 my-4 text-xl">
         {props.item}
-        <hr className='text-red-500' />
+        <hr className='' />
     </li>
     <span className='my-5'>
             <RiDeleteBin6Line
